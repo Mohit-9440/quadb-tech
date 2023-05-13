c92170ed4bafdc96cf0566470931154788240725
Mohit <mohitsharma8864@gmail.com>
2023-05-14 01:06:11 +0530

    summary page

---
+++ src/pages/showsummary/ShowSummary.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import axios from "axios";
+import './showsummary.css'
+function ShowSummary() {
+    const [show, setShow] = useState([]);
+    const { id } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`https://api.tvmaze.com/shows/${id}`)
+      .then((response) => {
+        setShow(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    }, [id]);
+
+  return (
+    <div className='summary-card-wrapper'> 
+        <div className='summary-card'>
+            {show.name && (
+                <>
+                    <h1>{show.name}</h1>
+                    <img src={show.image?.original} alt={show.name} />
+                    <p className='show-summary' dangerouslySetInnerHTML={{__html: show.summary}}></p>
+                    <Link to={{
+                        pathname: '/booking-form',
+                        state: { movieName: show.name }
+                        }}>
+                        <button className='book-ticket'>Book a Ticket</button>
+                    </Link>   
+                </>
+            )}       
+        </div>
+    </div>
+  );
+}
+export default ShowSummary;
+
